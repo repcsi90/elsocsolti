285727432964098c8a05fc2e3f0ea10f10e116af
repcsi90 <stumph.robert.andras@diak.szamalk-szalezi.hu>
2024-10-21 19:38:21 +0200

    kiirTomb(int[] Tomb

--- src/main/java/csolti/firstcsolti/Firstcsolti.java
+++ src/main/java/csolti/firstcsolti/Firstcsolti.java
@@ -4,13 +4,31 @@
 
 package csolti.firstcsolti;
 
-/**
- *
- * @author Robi
- */
+
 public class Firstcsolti {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+    System.out.println("eredeti: ");    
+        
+    int [] eredeti = {0,1,2,3};
+    kiirTomb(eredeti);
+    valtoztat(eredeti);
+    System.out.println("változatás után: ");
+    kiirTomb(eredeti);   
     }
+    
+    private static void kiirTomb(int [] Tomb){
+        for (int szam : Tomb){
+            System.out.println(szam + " ");
+        }
+        System.out.println("");
+    }
+
+    private static void valtoztat(int[] Tomb) {
+        Tomb[0]=100;
+    }
+    
+    
+    
 }
