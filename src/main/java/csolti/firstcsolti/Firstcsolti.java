df182645df7d87c2165d9e72701aa23420ba10e2
repcsi90 <stumph.robert.andras@diak.szamalk-szalezi.hu>
2024-10-21 19:14:39 +0200

    1.0

---
+++ src/main/java/csolti/firstcsolti/Firstcsolti.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package csolti.firstcsolti;
+
+/**
+ *
+ * @author Robi
+ */
+public class Firstcsolti {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
